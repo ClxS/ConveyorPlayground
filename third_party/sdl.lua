02c07e5c9a34a861d11f6a9bc119cb96dabb98c7
Carl <slxxls92@gmail.com>
2022-01-13 13:57:28 +0000

    Added correct files to SDL for windows

--- third_party/sdl.lua
+++ third_party/sdl.lua
@@ -2,16 +2,41 @@ project "SDL"
 	kind "None"
 	filter {"platforms:SDL"}
 		kind "StaticLib"
-	filter {}
+		links {
+			"winmm.lib",
+			"d3d11.lib",
+			"setupapi.lib",
+			"imm32.lib",
+			"version.lib",
+		}
+		includedirs {
+			"SDL/include",
+		}
+		files {
+			"SDL/include/*",
+			"SDL/src/*",
+			"SDL/src/*/*",
+			"SDL/src/*/windows/*",
+			"SDL/src/audio/wasapi/*",
+			"SDL/src/audio/directsound/*",
+			"SDL/src/audio/winmm/*",
+			"SDL/src/audio/disk/*",
+			"SDL/src/audio/dummy/*",
+			"SDL/src/joystick/virtual/*",
+			"SDL/src/joystick/hidapi/*",
+			"SDL/src/render/direct3d/*",
+			"SDL/src/render/direct3d11/*",
+			"SDL/src/render/opengl/*",
+			"SDL/src/render/opengles2/*",
+			"SDL/src/render/software/*",
+			"SDL/src/thread/generic/*",
+			"SDL/src/video/yuv2rgb/*",
+			"SDL/src/video/dummy/*",
+		}
 
-	includedirs {
-		"SDL/include",
-	}
-	files {
-		"SDL/include/*",
-		"SDL/src/*",
-		"SDL/src/*/*",
-		"SDL/src/*/windows/*",
-		"SDL/src/*/directsound/*",
-	}
-	flags { "MultiProcessorCompile" }
+		removefiles {
+			"**winrt.h",
+			"**winrt.cpp",
+		}
+		flags { "MultiProcessorCompile" }
+	filter {}
