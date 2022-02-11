0810d5131eaf3d85ba3ad2831f8df4b26828df37
Carl <slxxls92@gmail.com>
2022-02-11 20:02:36 +0000

    Added new AssetBuilder tool

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,5 @@
+require "ConveyorModule"
+
 workspace "CppConveyor"
 	location "build"
 	platforms { --[["Console",]] "SDL" }
@@ -15,80 +17,110 @@ workspace "CppConveyor"
 		intrinsics "on"
 	filter{}
 
-project "CppConveyor"
-	location "build/CppConveyor"
-	language "C++"
-	editandcontinue "On"
-	debugdir "$(TargetDir)"
-	files {
-		".editorconfig",
-		"data/**",
-		"src/**"
-	}
-	flags { "FatalWarnings" }
-
-	filter {"platforms:Console"}
+group("Tools")
+	project "AssetBuilder"
 		kind "ConsoleApp"
-		defines {
-			"_CONSOLE"
+		targetdir "bin/tools"
+		targetname "AssetBuilder"
+		location "build/AssetBuilder"
+		language "C++"
+		debugdir "$(TargetDir)"	
+		files {
+			"tools/AssetBuilder/src/**",
+			"tools/AssetBuilder/include/**",
+		}	
+		includedirs {
+			"tools/AssetBuilder/src/**",	
+			"tools/AssetBuilder/include/**",		
 		}
-		postbuildcommands {
-			'robocopy "' .. path.getabsolute("data/console") .. '" "$(TargetDir)data/platform" /E',
-			'robocopy "' .. path.getabsolute("data/common") .. '" "$(TargetDir)data" /E',
-			'exit /b 0'
+		flags { "FatalWarnings" }
+
+group("")
+	project "CppConveyor"
+		location "build/CppConveyor"
+		language "C++"
+		editandcontinue "On"
+		debugdir "$(TargetDir)"
+		files {
+			".editorconfig",
+			"data/**",
+			"src/**"
 		}
-		removefiles {
-			"src/Platform/SDL/**",
-		}
-	filter {"platforms:SDL"}
-		kind "WindowedApp"
-		defines {
-			"_SDL"
-		}
-		postbuildcommands {
-			'robocopy "' .. path.getabsolute("data/common") .. '" "$(TargetDir)data" /E',
-		}
-		if os.isdir(path.getabsolute('data/sdlPrivate')) then
-			print("Private assets exist, using those")
-			postbuildcommands {
-				'robocopy "' .. path.getabsolute("data/sdlPrivate") .. '" "$(TargetDir)data/platform" /E',
-				'exit /b 0'
-			}
-		else
-			postbuildcommands {
-				'robocopy "' .. path.getabsolute("data/sdl") .. '" "$(TargetDir)data/platform" /E',
-				'exit /b 0'
+		flags { "FatalWarnings" }
+
+		if _ACTION == 'vs2022' then
+			customtargets {
+				[[
+				  <Target Name="GenerateAssetSpec" BeforeTargets="InitializeBuildStatus">
+				  	<Message Text="Generating Asset Specification" Importance="High" />
+				  </Target>
+				]]
 			}
 		end
 
-		removefiles {
-			"src/Platform/Console/**",
-		}
-		includedirs {
-			"third_party/SDL/include",
-			"third_party/SDL_Image",
-			"third_party/imgui",
-		}
-		links {
-			"SDL",
-			"SDLImage",
-			"imgui",
-		}
-	filter {}
-	includedirs {
-		"src",
-		"src/**",
-	}
-	cppdialect "C++latest"
-	vpaths
-	{
-		["*"] = "src",
-		["data/*"] = "data/*",
-		["data/*"] = "data/common/**",
-		["data/platform/*"] = "data/console/**",
-	}
-	flags { "MultiProcessorCompile" }
+		filter {"platforms:Console"}
+			kind "ConsoleApp"
+			defines {
+				"_CONSOLE"
+			}
+			postbuildcommands {
+				'robocopy "' .. path.getabsolute("data/console") .. '" "$(TargetDir)data/platform" /E',
+				'robocopy "' .. path.getabsolute("data/common") .. '" "$(TargetDir)data" /E',
+				'exit /b 0'
+			}
+			removefiles {
+				"src/Platform/SDL/**",
+			}
+		filter {"platforms:SDL"}
+			kind "WindowedApp"
+			defines {
+				"_SDL"
+			}
+			postbuildcommands {
+				'robocopy "' .. path.getabsolute("data/common") .. '" "$(TargetDir)data" /E',
+			}
+			if os.isdir(path.getabsolute('data/sdlPrivate')) then
+				print("Private assets exist, using those")
+				postbuildcommands {
+					'robocopy "' .. path.getabsolute("data/sdlPrivate") .. '" "$(TargetDir)data/platform" /E',
+					'exit /b 0'
+				}
+			else
+				postbuildcommands {
+					'robocopy "' .. path.getabsolute("data/sdl") .. '" "$(TargetDir)data/platform" /E',
+					'exit /b 0'
+				}
+			end
 
-dofile('third_party/sdl.lua')
-dofile('third_party/sdlimage.lua')
-dofile('third_party/imgui.lua')
+			removefiles {
+				"src/Platform/Console/**",
+			}
+			includedirs {
+				"third_party/SDL/include",
+				"third_party/SDL_Image",
+				"third_party/imgui",
+			}
+			links {
+				"SDL",
+				"SDLImage",
+				"imgui",
+			}
+		filter {}
+		includedirs {
+			"src",
+			"src/**",
+		}
+		cppdialect "C++latest"
+		vpaths
+		{
+			["*"] = "src",
+			["data/*"] = "data/*",
+			["data/*"] = "data/common/**",
+			["data/platform/*"] = "data/console/**",
+		}
+		flags { "MultiProcessorCompile" }
+
+group("ThirdParty")
+	dofile('third_party/sdl.lua')
+	dofile('third_party/sdlimage.lua')
+	dofile('third_party/imgui.lua')
