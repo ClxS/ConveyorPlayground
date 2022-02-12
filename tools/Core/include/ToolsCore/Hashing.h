c572f13b30a6fdaa46cac167b4c423600650402f
Carl <slxxls92@gmail.com>
2022-02-12 10:19:57 +0000

    Added FNV1 64 bit hash to tools core

---
+++ tools/Core/include/ToolsCore/Hashing.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <functional>
+#include <string>
+#include <vector>
+
+namespace cppconv::tools::hashing
+{
+    static constexpr uint64_t fnv1(const std::string_view str)
+    {
+        uint64_t result = 0xcbf29ce484222325;
+        for (const char c : str)
+        {
+            result *= 1099511628211;
+            result ^= c;
+        }
+
+        return result;
+    }
+}
