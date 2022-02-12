b4ac5a09a70b66ac810cad150019b1922e2e929b
Carl <slxxls92@gmail.com>
2022-02-12 10:20:09 +0000

    Branch for generate-spec verb

--- tools/AssetBuilder/src/main.cpp
+++ tools/AssetBuilder/src/main.cpp
@@ -2,6 +2,9 @@
 
 #include "Arguments.h"
 #include "ExitCodes.h"
+#include "ToolsCore/Hashing.h"
+
+int doSpecGenerate(const Arguments&);
 
 int main(const int argc, char **argv)
 {
@@ -12,6 +15,18 @@ int main(const int argc, char **argv)
         return static_cast<int>(ExitCode::ArgumentParseError);
     }
 
+    using cppconv::tools::hashing::fnv1;
+
+    switch (fnv1(args.m_Mode.m_Value))
+    {
+        case fnv1("generate-spec"): return doSpecGenerate(args);
+    }
+
+    return 0;
+}
+
+int doSpecGenerate(const Arguments& args)
+{
     return 0;
 }
 
