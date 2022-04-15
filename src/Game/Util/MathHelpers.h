fe7942eded6cda7639b7ffcb82510b88c75d5caf
Carl <slxxls92@gmail.com>
2022-04-15 19:33:53 +0100

    Added rad/deg agnostic angle wrapper

---
+++ src/Game/Util/MathHelpers.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace cpp_conv::math_helpers
+{
+    constexpr float c_pi = 3.14159265358979323846f;
+
+    struct Angle
+    {
+        [[nodiscard]] float AsRadians() const { return m_Value; }
+        [[nodiscard]] float AsDegrees() const { return m_Value * (180.0f / c_pi); }
+
+        static Angle FromRadians(const float value)
+        {
+            Angle angle;
+            angle.SetRadians(value);
+            return angle;
+        }
+
+        static Angle FromDegrees(const float value)
+        {
+            Angle angle;
+            angle.SetDegrees(value);
+            return angle;
+        }
+
+        void SetRadians(const float value) { m_Value = value; }
+        void SetDegrees(const float value) { m_Value = value  * (c_pi / 180.0f); }
+
+    private:
+        float m_Value{};
+    };
+}
+
+inline cpp_conv::math_helpers::Angle operator ""_radians(long double value)
+{
+    return cpp_conv::math_helpers::Angle::FromRadians(static_cast<float>(value));
+}
+
+inline cpp_conv::math_helpers::Angle operator ""_degrees(long double value)
+{
+    return cpp_conv::math_helpers::Angle::FromDegrees(static_cast<float>(value));
+}
