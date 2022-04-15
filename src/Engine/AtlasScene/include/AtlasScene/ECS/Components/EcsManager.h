5516e9f62ce1845d8a4d0bc692ff503398383de8
Carl <slxxls92@gmail.com>
2022-04-15 19:33:38 +0100

    Added support for iterating components using structured binding

--- src/Engine/AtlasScene/include/AtlasScene/ECS/Components/EcsManager.h
+++ src/Engine/AtlasScene/include/AtlasScene/ECS/Components/EcsManager.h
@@ -13,61 +13,6 @@ namespace atlas::scene
 {
     class EcsManager
     {
-    public:
-        EcsManager();
-        EcsManager(const EcsManager&) = delete;
-        EcsManager(const EcsManager&&) = delete;
-        EcsManager operator=(const EcsManager&) = delete;
-        EcsManager operator=(const EcsManager&&) = delete;
-
-        ~EcsManager();
-
-        EntityId AddEntity();
-
-        void RemoveEntity(EntityId entity);
-
-        template <typename TComponent, typename... TArgs>
-        TComponent& AddComponent(EntityId entity, TArgs&& ...args);
-
-        template <typename TComponent>
-        TComponent& AddComponent(EntityId entity, TComponent&& value);
-
-        template <typename TComponent>
-        void RemoveComponent(const EntityId entity);
-
-        template <typename TComponent>
-        [[nodiscard]] bool DoesEntityHaveComponent(EntityId entity) const;
-
-        template <typename TComponent, typename... TOtherComponents>
-        bool DoesEntityHaveComponents(EntityId entity) const;
-
-
-        [[nodiscard]] std::vector<EntityId> GetEmptyEntities() const
-        {
-            return m_ArchetypePools[0].m_EntityPool.GetData();
-        }
-
-        template <typename TComponent, typename... TOtherComponents>
-        [[nodiscard]] std::vector<EntityId> GetEntitiesWithComponents() const;
-
-        template <typename TComponent>
-        [[nodiscard]] TComponent& GetComponent(EntityId entityId);
-
-        template <typename TComponent>
-        [[nodiscard]] const TComponent& GetComponent(EntityId entityId) const;
-
-        template <typename... TComponent>
-        [[nodiscard]] std::tuple<TComponent&...> GetComponents(EntityId entityId);
-
-        template <typename... TComponent>
-        [[nodiscard]] std::tuple<const TComponent&...> GetComponents(EntityId entityId) const;
-
-        template <typename... TComponent>
-        void ForEachComponents(std::function<void(EntityId, TComponent&...)> callback);
-
-        template <typename... TComponent>
-        void ForEachComponents(std::function<bool(EntityId, const TComponent&...)> callback) const;
-
     private:
         struct MaskedComponentPool
         {
@@ -104,6 +49,118 @@ namespace atlas::scene
             }
         };
 
+    public:
+        template <typename... TComponents>
+        struct ComponentIterator
+        {
+            using iterator_category = std::forward_iterator_tag;
+            using difference_type   = std::ptrdiff_t;
+            using value_type        = std::tuple<EntityId, TComponents&...>;
+
+            explicit ComponentIterator(std::vector<ArchetypePool>* archetypePools,uint64_t mask);
+            ComponentIterator(std::vector<ArchetypePool>* archetypePools, const int32_t pool, int32_t entity, uint64_t mask);
+
+            // Prefix increment
+            ComponentIterator& operator++();
+
+            // Postfix increment
+            ComponentIterator operator++(int);
+
+            friend bool operator== (const ComponentIterator& a, const ComponentIterator& b)
+            {
+                return a.m_PoolIndex == b.m_PoolIndex &&
+                    a.m_PoolEntityIndex == b.m_PoolEntityIndex &&
+                    a.m_Mask == b.m_Mask;
+            }
+            friend bool operator!= (const ComponentIterator& a, const ComponentIterator& b)
+            {
+                return !(a == b);
+            }
+
+            value_type operator*() const;
+
+        private:
+            void FindNextEntity();
+
+            template<typename TComponent>
+            static TComponent& GetComponentFromPool(ArchetypePool& pool, int32_t entityIndex);
+
+            int32_t m_PoolIndex;
+            int32_t m_PoolEntityIndex;
+            uint64_t m_Mask;
+            std::vector<ArchetypePool>* m_ArchetypePools;
+        };
+
+        template <typename... TComponents>
+        struct ComponentIteratorWrapper
+        {
+            ComponentIteratorWrapper(std::vector<ArchetypePool>* archetypePools);
+            ComponentIterator<TComponents...> begin();
+            ComponentIterator<TComponents...> end();
+
+            uint64_t m_Mask;
+            std::vector<ArchetypePool>* m_ArchetypePools;
+        };
+
+        EcsManager();
+        EcsManager(const EcsManager&) = delete;
+        EcsManager(const EcsManager&&) = delete;
+        EcsManager operator=(const EcsManager&) = delete;
+        EcsManager operator=(const EcsManager&&) = delete;
+
+        ~EcsManager();
+
+        EntityId AddEntity();
+
+        void RemoveEntity(EntityId entity);
+
+        template <typename TComponent, typename... TArgs>
+        TComponent& AddComponent(EntityId entity, TArgs&& ...args);
+
+        template <typename TComponent>
+        TComponent& AddComponent(EntityId entity, TComponent&& value);
+
+        template <typename TComponent>
+        void RemoveComponent(const EntityId entity);
+
+        template <typename TComponent>
+        [[nodiscard]] bool DoesEntityHaveComponent(EntityId entity) const;
+
+        template <typename TComponent, typename... TOtherComponents>
+        [[nodiscard]] bool DoesEntityHaveComponents(EntityId entity) const;
+
+        [[nodiscard]] std::vector<EntityId> GetEmptyEntities() const
+        {
+            return m_ArchetypePools[0].m_EntityPool.GetData();
+        }
+
+        template <typename TComponent, typename... TOtherComponents>
+        [[nodiscard]] std::vector<EntityId> GetEntitiesWithComponents() const;
+
+        template <typename TComponent>
+        [[nodiscard]] TComponent& GetComponent(EntityId entityId);
+
+        template <typename TComponent>
+        [[nodiscard]] const TComponent& GetComponent(EntityId entityId) const;
+
+        template <typename... TComponent>
+        [[nodiscard]] std::tuple<TComponent&...> GetComponents(EntityId entityId);
+
+        template <typename... TComponent>
+        [[nodiscard]] std::tuple<const TComponent&...> GetComponents(EntityId entityId) const;
+
+        template <typename... TComponent>
+        [[nodiscard]] ComponentIteratorWrapper<TComponent...> IterateEntityComponents();
+
+        template <typename... TComponent>
+        void ForEachComponents(std::function<void(EntityId, TComponent&...)> callback);
+
+        template <typename... TComponent>
+        void ForEachComponents(std::function<bool(EntityId, const TComponent&...)> callback) const;
+
+
+    private:
+
         template <typename...>
         struct MaskLookup;
 
@@ -130,7 +187,7 @@ namespace atlas::scene
             return m_ArchetypePools[index.m_ArchetypeIndex];
         }
 
-        const ArchetypePool& GetPool(const ArchetypeIndex index) const
+        [[nodiscard]] const ArchetypePool& GetPool(const ArchetypeIndex index) const
         {
             return m_ArchetypePools[index.m_ArchetypeIndex];
         }
@@ -353,6 +410,12 @@ namespace atlas::scene
     }
 
     template <typename ... TComponent>
+    EcsManager::ComponentIteratorWrapper<TComponent...> EcsManager::IterateEntityComponents()
+    {
+        return EcsManager::ComponentIteratorWrapper<TComponent...>{&m_ArchetypePools};
+    }
+
+    template <typename ... TComponent>
     void EcsManager::ForEachComponents(std::function<void(EntityId, TComponent&...)> callback)
     {
         for (auto entity : GetEntitiesWithComponents<TComponent...>())
@@ -370,6 +433,107 @@ namespace atlas::scene
             {
                 break;
             }
+        }
+    }
+
+    template <typename ... TComponents>
+    EcsManager::ComponentIterator<TComponents...>::ComponentIterator(std::vector<ArchetypePool>* archetypePools, const uint64_t mask)
+        : m_PoolIndex{-1}
+        , m_PoolEntityIndex{-1}
+        , m_Mask{mask}
+        , m_ArchetypePools{archetypePools}
+    {
+        m_PoolIndex = -1;
+        m_PoolEntityIndex = -1;
+        //FindNextEntity();
+    }
+
+    template <typename ... TComponents>
+    EcsManager::ComponentIterator<TComponents...>::ComponentIterator(std::vector<ArchetypePool>* archetypePools, const int32_t pool, const int32_t entity, const uint64_t mask)
+        : m_PoolIndex{pool}
+        , m_PoolEntityIndex{entity}
+        , m_Mask{mask}
+        , m_ArchetypePools{archetypePools}
+    {
+    }
+
+    template <typename ... TComponents>
+    EcsManager::ComponentIterator<TComponents...>& EcsManager::ComponentIterator<TComponents...>::operator++()
+    {
+        //FindNextEntity();
+        return *this;
+    }
+
+    template <typename ... TComponents>
+    EcsManager::ComponentIterator<TComponents...> EcsManager::ComponentIterator<TComponents...>::operator++(int)
+    { ComponentIterator tmp = *this; ++(*this); return tmp; }
+
+    template <typename ... TComponents>
+    typename EcsManager::ComponentIterator<TComponents...>::value_type EcsManager::ComponentIterator<TComponents...>::
+    operator*() const
+    {
+        auto& pool = (*m_ArchetypePools)[m_PoolIndex];
+        EntityId entity = pool.m_EntityPool.GetCopy(m_PoolEntityIndex);
+
+        return { entity, GetComponentFromPool<TComponents>(pool, m_PoolEntityIndex)... };
+    }
+
+    template <typename ... TComponents>
+    template <typename TComponent>
+    TComponent& EcsManager::ComponentIterator<TComponents...>::GetComponentFromPool(ArchetypePool& pool, int32_t entityIndex)
+    {
+        const auto& componentPool = pool.GetComponentPool(ComponentRegistry::GetComponentMask<TComponent>());
+        ComponentPool<TComponent>* pTypedPool = static_cast<ComponentPool<TComponent>*>(componentPool.m_Pool);
+        return pTypedPool->GetReference(entityIndex);
+    }
+
+    template <typename ... TComponents>
+    EcsManager::ComponentIteratorWrapper<TComponents...>::ComponentIteratorWrapper(std::vector<ArchetypePool>* archetypePools)
+        : m_ArchetypePools{archetypePools}
+    {
+        m_Mask = MaskLookup<TComponents...>::GetComponentMask();
+    }
+
+    template <typename ... TComponents>
+    EcsManager::ComponentIterator<TComponents...> EcsManager::ComponentIteratorWrapper<TComponents...>::begin()
+    {
+        return ComponentIterator<TComponents...>(m_ArchetypePools, m_Mask);
+    }
+
+    template <typename ... TComponents>
+    EcsManager::ComponentIterator<TComponents...> EcsManager::ComponentIteratorWrapper<TComponents...>::end()
+    {
+        return ComponentIterator<TComponents...>(m_ArchetypePools, -1, -1, m_Mask);
+    }
+
+    template <typename ... TComponent>
+    void EcsManager::ComponentIterator<TComponent...>::FindNextEntity()
+    {
+        if (m_PoolIndex >= 0)
+        {
+            m_PoolEntityIndex++;
+            if (m_PoolEntityIndex < (*m_ArchetypePools)[m_PoolIndex].m_EntityPool.Size())
+            {
+                return;
+            }
+        }
+
+        bool found = false;
+        m_PoolIndex++;
+        for (;m_PoolIndex < m_ArchetypePools->size(); m_PoolIndex++)
+        {
+            auto& archetype = (*m_ArchetypePools)[m_PoolIndex];
+            if (archetype.m_EntityPool.Size() > 0 && (archetype.m_ArchetypeComponentMask & m_Mask) == m_Mask)
+            {
+                m_PoolEntityIndex = 0;
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            m_PoolIndex = -1;
+            m_PoolEntityIndex = -1;
         }
     }
 
