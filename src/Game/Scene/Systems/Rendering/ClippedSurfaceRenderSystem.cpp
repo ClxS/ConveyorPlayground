7c4140b0ecd128941a1cfcf9fbb11a4b00665b73
Carl <slxxls92@gmail.com>
2022-05-17 19:45:37 +0100

    Added test polyhedron surface drawing

--- src/Game/Scene/Systems/Rendering/ClippedSurfaceRenderSystem.cpp
+++ src/Game/Scene/Systems/Rendering/ClippedSurfaceRenderSystem.cpp
@@ -1,15 +1,100 @@
 #include "ClippedSurfaceRenderSystem.h"
 
+#include "AssetRegistry.h"
 #include "ModelComponent.h"
 #include "PositionComponent.h"
 #include "AtlasRender/Renderer.h"
 #include "AtlasRender/AssetTypes/ModelAsset.h"
+#include "AtlasResource/ResourceLoader.h"
 #include "Eigen/Core"
+#include "Geometry/Polyhedra/Polyhedron.h"
+
+namespace
+{
+    bgfx::VertexBufferHandle polyhedraVertices;
+    bgfx::IndexBufferHandle polyhedraIndices;
+
+    void createPolyhedra()
+    {
+        const auto polyhedra = cpp_conv::util::geometry::polyhedron::createPolyhedron(50, 0, 2.0f);
+
+        bgfx::VertexLayout vertexLayout;
+        vertexLayout
+            .begin()
+            .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+            .end();
+
+        const auto& points = polyhedra.GetPoints();
+        const auto& triangles = polyhedra.GetTriangles();
+        const auto& squares = polyhedra.GetSquares();
+
+        const int vertexCount = static_cast<int>(points.size());
+        const auto indexCount = static_cast<uint32_t>(triangles.size() * 3 + squares.size() * 6);
+
+        const bool use16BitIndex = points.size() < 65535;
+        const int indexBufferStride = use16BitIndex ? 2 : 4;
+
+        const auto bufferSize = vertexLayout.getSize(vertexCount);
+        const bgfx::Memory* vertexMemory = bgfx::alloc(bufferSize);
+        const bgfx::Memory* indexMemory = bgfx::alloc(indexCount * indexBufferStride);
+
+        std::memcpy(vertexMemory->data, points.data(), bufferSize);
+
+        if (use16BitIndex)
+        {
+            int offset = 0;
+            auto* indexBuffer = reinterpret_cast<uint16_t*>(indexMemory->data);
+            for(const auto& square : squares)
+            {
+                indexBuffer[offset++] = static_cast<uint16_t>(square.m_Indices[0]);
+                indexBuffer[offset++] = static_cast<uint16_t>(square.m_Indices[1]);
+                indexBuffer[offset++] = static_cast<uint16_t>(square.m_Indices[2]);
+                indexBuffer[offset++] = static_cast<uint16_t>(square.m_Indices[0]);
+                indexBuffer[offset++] = static_cast<uint16_t>(square.m_Indices[3]);
+                indexBuffer[offset++] = static_cast<uint16_t>(square.m_Indices[2]);
+            }
+
+            for(const auto& triangle : triangles)
+            {
+                indexBuffer[offset++] = static_cast<uint16_t>(triangle.m_Indices[0]);
+                indexBuffer[offset++] = static_cast<uint16_t>(triangle.m_Indices[2]);
+                indexBuffer[offset++] = static_cast<uint16_t>(triangle.m_Indices[1]);
+            }
+        }
+        else
+        {
+            int offset = 0;
+            auto* indexBuffer = reinterpret_cast<uint32_t*>(indexMemory->data);
+            for(const auto& square : squares)
+            {
+                indexBuffer[offset++] = static_cast<uint32_t>(square.m_Indices[0]);
+                indexBuffer[offset++] = static_cast<uint32_t>(square.m_Indices[1]);
+                indexBuffer[offset++] = static_cast<uint32_t>(square.m_Indices[2]);
+                indexBuffer[offset++] = static_cast<uint32_t>(square.m_Indices[0]);
+                indexBuffer[offset++] = static_cast<uint32_t>(square.m_Indices[3]);
+                indexBuffer[offset++] = static_cast<uint32_t>(square.m_Indices[2]);
+            }
+
+            for(const auto& triangle : triangles)
+            {
+                indexBuffer[offset++] = static_cast<uint32_t>(triangle.m_Indices[0]);
+                indexBuffer[offset++] = static_cast<uint32_t>(triangle.m_Indices[2]);
+                indexBuffer[offset++] = static_cast<uint32_t>(triangle.m_Indices[1]);
+            }
+        }
+
+        polyhedraVertices = bgfx::createVertexBuffer(vertexMemory, vertexLayout);
+        polyhedraIndices = bgfx::createIndexBuffer(indexMemory, use16BitIndex ? BGFX_BUFFER_NONE : BGFX_BUFFER_INDEX32);
+    }
+}
 
 void cpp_conv::ClippedSurfaceRenderSystem::Initialise(atlas::scene::EcsManager&,
                                                       const uint8_t surfaceRenderMask,
                                                       const uint8_t clipCasterRenderMask)
 {
+    createPolyhedra();
+
+
     m_Masks.m_SurfaceRenderMask = surfaceRenderMask;
     m_Masks.m_ClipCasterRenderMask = clipCasterRenderMask;
     m_Uniforms.m_SurfaceColour = createUniform("u_surfaceColour", bgfx::UniformType::Vec4);
@@ -68,4 +153,19 @@ void cpp_conv::ClippedSurfaceRenderSystem::Update(atlas::scene::EcsManager& ecs)
             Eigen::Matrix4f::Identity(),
             ~BGFX_DISCARD_STATE);
     }
+
+    bgfx::setState(0
+                    | BGFX_STATE_WRITE_RGB
+                    | BGFX_STATE_WRITE_A
+                    | BGFX_STATE_WRITE_Z
+                    | BGFX_STATE_DEPTH_TEST_LESS
+                    | BGFX_STATE_FRONT_CCW
+                    | BGFX_STATE_MSAA);
+    draw(
+        constants::render_views::c_geometry,
+        polyhedraVertices,
+        polyhedraIndices,
+        atlas::resource::ResourceLoader::LoadAsset<resources::registry::CoreBundle, atlas::render::ShaderProgram>(resources::registry::core_bundle::shaders::c_basicUntexturedUninstanced),
+        Eigen::Matrix4f::Identity(),
+        ~BGFX_DISCARD_STATE);
 }
