3c1d1403103d97c32bb89f08888b8dc57675cfc0
Carl <slxxls92@gmail.com>
2022-08-10 18:57:54 +0100

    Added atlas module

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-require "ConveyorModule"
+include("src/Atlas")
 
 workspace "CppConveyor"
 	location "build"
@@ -37,42 +37,11 @@ workspace "CppConveyor"
 		}
 	}
 
-group("Tools")
-    include("src/Tools/Core")
-    include("src/Tools/AssetBuilder")
-
-group("Engine")
-    include("src/Engine/AtlasAppHost")
-    include("src/Engine/AtlasCore")
-    include("src/Engine/AtlasGame")
-    include("src/Engine/AtlasInput")
-    include("src/Engine/AtlasMaths")
-    include("src/Engine/AtlasRender")
-    include("src/Engine/AtlasResource")
-    include("src/Engine/AtlasScene")
-
-group("Engine/Tests")
-    include("src/Engine/Tests/AtlasSceneTests")
-
+setEngineRoot("src/Atlas")
+includeEngine()
+includeEngineTests()
+includeEngineTools()
+includeEngineThirdParty()
 
 group("")
 	include("src/Game")
-
-
-group("ThirdParty")
-	include('src/ThirdParty/benchmark.lua')
-	include('src/ThirdParty/eigen.lua')
-	include('src/ThirdParty/fixed_string.lua')
-	include('src/ThirdParty/freetype.lua')
-	include('src/ThirdParty/gtest.lua')
-	include('src/ThirdParty/imgui.lua')
-	include('src/ThirdParty/rmlui.lua')
-	include('src/ThirdParty/sdl.lua')
-	include('src/ThirdParty/sdlimage.lua')
-	include('src/ThirdParty/tomlcpp.lua')
-	include('src/ThirdParty/fmt.lua')
-
-group("ThirdParty/bgfx")
-	include('src/ThirdParty/bgfx.lua')
-	include('src/ThirdParty/bimg.lua')
-	include('src/ThirdParty/bx.lua')
